2626e3b29eea20245a8a5b71f4bbd54f7f007462
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2021-03-20 13:50:04 +0100

    First commit

---
+++ app/src/main/java/com/cbellmont/solucionexameneval1/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.cbellmont.solucionexameneval1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.SearchView
+import com.cbellmont.solucionexameneval1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    private val list = listOf("Alumno 1","Alumno 2","Alumno 3","Profesor 1","Profesor 2")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.swSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                Log.d(MainActivity::class.java.name, "onQueryTextSubmit ha recibido $p0")
+                p0?.let {
+                    binding.tvResult.text = list.filter { it.contains(p0) }.toString()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                Log.d(MainActivity::class.java.name, "onQueryTextChange ha recibido $p0")
+                p0?.let {
+                    binding.tvResult.text = list.filter { it.contains(p0) }.toString()
+                }
+                return true
+            }
+        })
+
+        binding.tvResult.text = list.toString()
+    }
+}
